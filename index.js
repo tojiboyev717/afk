a95de9303cc0cf5a416dc45ce23672083f473807
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-04-15 00:59:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 require('./keep_alive') // Serverni jonli ushlab turadi
 
-// 1,2 va 6 har 5 sekundda qayta ishga tushiriladi
+// 1.js, 2.js va 6.js fayllarini har 5 sekundda qayta ishga tushirish
 setInterval(() => {
   try {
     delete require.cache[require.resolve('./1')]
     require('./1')
+    console.log('1.js qayta ishga tushdi')
   } catch (err) {
+    console.error('1.js yuklashda xatolik:', err.message)
   }
 
   try {
     delete require.cache[require.resolve('./2')]
     require('./2')
+    console.log('2.js qayta ishga tushdi')
   } catch (err) {
+    console.error('2.js yuklashda xatolik:', err.message)
   }
-}, 5000) // 5 sekundda bir qayta yuklanadi
 
-try {
+  try {
     delete require.cache[require.resolve('./6')]
     require('./6')
+    console.log('6.js qayta ishga tushdi')
   } catch (err) {
+    console.error('6.js yuklashda xatolik:', err.message)
   }
-}, 5000) // 5 sekundda bir qayta yuklanadi
+}, 5000) // Har 5 sekundda qayta ishlaydi

6fdfb582eafc34bddba28198d14fc7188edeee6d
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-04-18 17:29:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('./2'); // Keep-alive serverni ishga tushirish
 // require('./4'); // Keep-alive serverni ishga tushirish
 require('./6'); // Keep-alive serverni ishga tushirish
 require('./glass'); // Keep-alive serverni ishga tushirish
+require('./nezer1'); // Keep-alive serverni ishga tushirish
+require('./nezer2'); // Keep-alive serverni ishga tushirish
+require('./nezer_buy'); // Keep-alive serverni ishga tushirish
+
 
 
 

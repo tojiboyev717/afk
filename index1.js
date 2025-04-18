9522ce4253b18607c2ff297f63b1dd706f3971ec
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-04-18 23:45:34 +0500

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,4 +1,4 @@
 require('./keep_alive'); // Keep-alive serverni ishga tushirish
 require('./nezer1'); // Keep-alive serverni ishga tushirish
 require('./nezer2'); // Keep-alive serverni ishga tushirish
-require('./nezer_buy'); // Keep-alive serverni ishga tushirish
+//require('./nezer_buy'); // Keep-alive serverni ishga tushirish

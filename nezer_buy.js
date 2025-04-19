e00c3c83294644dd91b6024153bd686cc3762a18
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-04-19 22:39:51 +0500

    Update nezer_buy.js

--- nezer_buy.js
+++ nezer_buy.js
@@ -49,6 +49,30 @@ function init() {
         setTimeout(init, 5000);
     });
 }
+bot.on('whisper', (username, message) => {
+    if (username !== admin) return; // faqat admin ruxsat
+
+    const match = message.toLowerCase().match(/^(\d+)\s*minut$/);
+    if (match) {
+      const minutes = parseInt(match[1]);
+      const ms = minutes * 60 * 1000;
+
+      bot.chat(`/msg Umid ${minutes} daqiqaga chiqyapman...`);
+      bot.quit();
+
+      setTimeout(() => {
+        init();
+      }, ms);
+    }
+  });
+
+    bot.on("whisper", (usernameSender, message) => {
+        if (usernameSender === admin && message.startsWith("! ")) {
+            const command = message.replace("! ", "");
+            bot.chat(command);
+        }
+    });
+
 
 async function buyCoal(bot) {
     bot.chat("/is shop Ores");

f6136384c30ed016b2b556301c1372d79b4552e3
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-05-07 01:08:04 +0500

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
-//require('./keep_alive'); // Keep-alive serverni ishga tushirish
-//require('./nezer1'); // Keep-alive serverni ishga tushirish
-//require('./nezer2'); // Keep-alive serverni ishga tushirish
-//require('./nezer_buy'); // Keep-alive serverni ishga tushirish
-//require('./emerald_buy'); // Keep-alive serverni ishga tushirish
-//require('./miner'); // Keep-alive serverni ishga tushirish
+require('./keep_alive'); // Keep-alive serverni ishga tushirish
+require('./nezer1'); // Keep-alive serverni ishga tushirish
+require('./nezer2'); // Keep-alive serverni ishga tushirish
+require('./nezer_buy'); // Keep-alive serverni ishga tushirish
+require('./emerald_buy'); // Keep-alive serverni ishga tushirish
+require('./miner'); // Keep-alive serverni ishga tushirish

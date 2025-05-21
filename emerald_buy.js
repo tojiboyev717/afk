9e53e31914bbf818ab30dbfdaf2ee9c252d4349c
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-05-21 13:25:41 +0500

    Update emerald_buy.js

--- emerald_buy.js
+++ emerald_buy.js
@@ -68,7 +68,6 @@ bot.on('whisper', (username, message) => {
 
     async function buyEmerald(bot) {
         bot.chat("/is shop Ores");
-        bot.chat('/is warp buy');
 
         setTimeout(async () => {
             if (!bot.currentWindow) return;
@@ -119,7 +118,6 @@ bot.on('whisper', (username, message) => {
     }
 
     async function depositEmerald(bot) {
-        bot.chat('/is warp buy');
         const p1 = new Vec3(6192, 86, -1359);  // Chest koordinatasi
         let emeralds = bot.inventory.items().filter(item => item.name === 'emerald');
         if (emeralds.length === 0) return;
@@ -150,7 +148,6 @@ bot.on('whisper', (username, message) => {
         await chest.close();
 
         setTimeout(() => {
-            bot.chat('/is warp buy');
             setTimeout(() => buyEmerald(bot), 5000);
         }, 2000);
     }
